ee9db27f64b612ee24d370231c0501b134afaad8
Andreas Pelme <andreas@pelme.se>
2016-09-22 17:43:10 +0200

    Basic tox/pytest setup.

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -1,10 +1,7 @@
 import fdfgen
-from unittest import TestCase
 
 
-class Tests(TestCase):
-
-    def test_string_with_unbalanced_paren(self):
-        s = 'a) 1st item'
-        e = b'\xfe\xff\x00a\x00\\)\x00 \x001\x00s\x00t\x00 \x00i\x00t\x00e\x00m'
-        self.assertEqual(fdfgen.smart_encode_str(s), e)
+def test_string_with_unbalanced_paren():
+    s = 'a) 1st item'
+    e = b'\xfe\xff\x00a\x00\\)\x00 \x001\x00s\x00t\x00 \x00i\x00t\x00e\x00m'
+    assert fdfgen.smart_encode_str(s) == e
