81ef680b5fc248ee02f5c54fea3dfe0154271d52
spooner <bil.bagpuss@gmail.com>
2017-02-17 23:58:20 +0000

    fixed tests

--- tests/test_fdf_identifier.py
+++ tests/test_fdf_identifier.py
@@ -3,16 +3,11 @@ import fdfgen
 
 def test_identifier_with_slash():
     expected_identifier = b'/Off'
-    result = fdfgen.FDFIdentifier('/Off')
+    result = fdfgen.FDFIdentifier('/Off').value
     assert result == expected_identifier
 
 
 def test_identifier_without_slash():
     expected_identifier = b'/Off'
-    result = fdfgen.FDFIdentifier('Off')
+    result = fdfgen.FDFIdentifier('Off').value
     assert result == expected_identifier
-
-
-test_identifier_without_slash()
-test_identifier_with_slash()
-print "hello"
